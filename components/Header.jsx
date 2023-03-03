2dcfa9b2586eb3e8106bb535819a5f32e87f52b6
Charles Karlo Leguarda <charlesleguarda@gmail.com>
2023-03-03 10:54:52 +0800

    First Commit
    In this project I used Nextjs,TailwindCSS,React Icons,

---
+++ components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='flex justify-between px-4 pt-4 font-bold'>
+        <h2>Dashboard</h2>
+        <h2>Welcome Back, Charles</h2>
+    </div>
+  )
+}
+
+export default Header
---
+++ data/data.js
@@ -0,0 +1,112 @@
+export const data = [
+    {
+        id: 1,
+        name: {
+            first: 'Charles',
+            last: 'Leguarda',
+        },
+        total: 2795.95,
+        status: 'On Hold',
+        method: 'Paypal',
+        date: '15 minutes ago'
+    },
+    {
+        id: 2,
+        name: {
+            first: 'Master Neil',
+            last: 'Lagrimas',
+        },
+        total: 1195.95,
+        status: 'Processing',
+        method: 'Paypal',
+        date: '23 Minutes ago'
+    },
+    {
+        id: 3,
+        name: {
+            first: 'Aubrey',
+            last: 'Pescadero',
+        },
+        total: 495.85,
+        status: 'Completed',
+        method: 'Completed',
+        date: '1 Hour Ago'
+    },
+    {
+        id: 4,
+        name: {
+            first: 'Kate',
+            last: 'Bermoy',
+        },
+        total: 150.45,
+        status: 'Processing',
+        method: 'MasterCard',
+        date: '1 Hour Ago',
+    },
+    {
+        id: 5,
+        name: {
+            first: 'Joana',
+            last: 'Compendio', 
+        },
+        total: 175.25,
+        status: 'On Hold',
+        method: 'Paypal',
+        date: '2 Hour ago',
+    },
+    {
+        id: 6,
+        name: {
+            first: 'Krisha',
+            last: 'Bermoy',
+        },
+        total: 1295.75 ,
+        status: 'Completed',
+        method: 'Check',
+        date: '3 Hour ago',
+    },
+    {
+        id: 7,
+        name: {
+            first: 'Alexandra',
+            last: 'Edyesca',
+        },
+        total: 89.95,
+        status: 'Completed',
+        method: 'MasterCard',
+        date: '3 Hour ago',
+    },
+    {
+        id: 8,
+        name: {
+            first: 'Hazel',
+            last: 'Sible',
+        },
+        total: 689.45,
+        status: 'Completed',
+        method: 'Visa',
+        date: '7 Hour ago',
+    },
+    {
+        id: 9,
+        name: {
+            first: 'Jericho ',
+            last: 'Cachuela',
+        },
+        total: 14.99,
+        status: 'Completed',
+        method: 'Paypal',
+        date: '1 Day ago',
+    },
+    {
+        id: 10,
+        name: {
+            first: 'Ricky',
+            last: 'Lanchinebre',
+        },
+        total: 218.99,
+        status: 'Completed',
+        method: 'MasterCard',
+        date: '1 Day ago',
+    },
+]
